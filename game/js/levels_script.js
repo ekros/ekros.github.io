30b93410dade261754d93f5f52d95ca9098f7316
Eric Ros <ekros@outlook.com>
2019-06-12 22:40:48 +0200

    Updates

---
+++ game/js/levels_script.js
@@ -0,0 +1,51 @@
+var levels_script = [
+{
+  // reserved for main menu
+},
+{
+  "opening_speech":[
+    "Hi! My name is Eric.",
+    "I'm a level 33 human web developer.",
+    "Someone stole my resume. Will you help me?",
+    "These monsters seem very suspicious!",
+    "Use the arrow keys to move and the space bar to jump!"
+  ],
+  "ending_speech":[
+    "I found my resume.. well, some parts of it.",
+    "I have to find more monsters!"
+  ],
+  "resume_pieces":[
+    "SKILLS.. HTML / CSS / JS / AngularJS",
+    "Ruby on Rails"
+  ]
+},
+{
+  "opening_speech":[
+    "Hi again!"
+  ],
+  "ending_speech":[
+    "Well done!"
+  ],
+  "resume_pieces":[
+    "EXPERIENCE..",
+    "ACB - Front End developer and sysadmin",
+    "Abiquo - Java / C++ developer",
+    "Acer Computer - Technician"
+  ]
+},
+{
+  "opening_speech":[
+    "It is almost done. Ready!"
+  ],
+  "ending_speech":[
+    "I recovered all the pieces! Jump under the button to download the resume!"
+  ],
+  "resume_pieces":[
+    "INTERESTS...",
+    "Front End dev...",
+    "Mobile dev...",
+    "Gamification",
+    "Design"
+  ]
+}
+]
---
+++ styles-2b1e4335cbe084bcca93.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[0],{148:function(n,o,w){},150:function(n,o,w){}}]);
+//# sourceMappingURL=styles-2b1e4335cbe084bcca93.js.map
